eb0f098d9a2c8e347fdc101507ec5201bbec3a87
M.Sc. Damir Olejar <olejar.damir@gmail.com>
2020-12-19 13:13:27 -0500

    Update HanoiController.java

--- src/main/java/controller/HanoiController.java
+++ src/main/java/controller/HanoiController.java
@@ -19,14 +19,32 @@ import service.TowersService;
 
     public Result newGame(@PathParam("rods") int rods,
                           @PathParam("pegs") int pegs) {
-        towersService = new TowersService();
-        answerModel = towersService.newGame(pegs, rods);
-        return Results.json().render(answerModel);
+        try {
+            towersService = new TowersService();
+            answerModel = towersService.newGame(pegs, rods);
+            return Results.json().render(answerModel);
+        }catch (Exception e){
+            AnswerModel exception = new AnswerModel();
+            exception.setMessage("Exception caught");
+            if(towersService!=null){
+                exception.setCurrentState(towersService.getTowersModel());
+            }
+            return Results.json().render(exception);
+        }
     }
 
     public Result playGame(@PathParam("from") int from,
                            @PathParam("to") int to) {
-        answerModel = towersService.makeMove(from, to);
-        return Results.json().render(answerModel);
+        try {
+            answerModel = towersService.makeMove(from, to);
+            return Results.json().render(answerModel);
+        }catch (Exception e){
+            AnswerModel exception = new AnswerModel();
+            exception.setMessage("Illegal move, cannot move from "+from+" to "+to+".");
+            if(towersService!=null){
+                exception.setCurrentState(towersService.getTowersModel());
+            }
+            return Results.json().render(exception);
+        }
     }
 }
